86a4fa1e36fc6128052b2586cbad80a4ddb210a3
Muhammad Nasir <mn660875@gmail.com>
2025-08-01 16:20:42 +0500

    savor

--- src/app/components/Dashboard.js
+++ src/app/components/Dashboard.js
@@ -6,7 +6,7 @@ export default  function Dashboard(){
     return(
         <div className=" flex items-center justify-center  flex-col   mt-8">
          
-         <FlipLink href="#" className=" text-lg font-bold py-3">Savour&nbsp;Sprint</FlipLink>
+         <FlipLink href="#" className=" text-lg font-bold py-3">Savor&nbsp;Sprint</FlipLink>
             <div className="text-center">
           
                 <p className="text-gray-500 mt-2">A blog about food, experience and recipes.</p>
